e01bee88da7548f2fbab81e3c922e25adf9d740a
kingwel.xie@139.com <kingwel.xie@139.com>
2024-03-26 20:02:36 +0800

    initial commit

---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import "github.com/kingwel-xie/go-bitget/constants"
+
+const (
+	BaseUrl = "https://api.bitget.com"
+	WsUrl   = "wss://ws.bitget.com/mix/v1/stream"
+
+	ApiKey        = ""
+	SecretKey     = ""
+	PASSPHRASE    = ""
+	TimeoutSecond = 30
+	SignType      = constants.SHA256
+)
